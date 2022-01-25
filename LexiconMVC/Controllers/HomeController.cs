12808c8f1fb3116fb2f65ac2dd3608c909fad914
Vingnir <vingnir@live.se>
2022-01-25 05:36:00 +0100

    Add project files.

---
+++ LexiconMVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
