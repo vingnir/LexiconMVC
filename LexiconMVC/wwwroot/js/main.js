cb046dbdd5d555809b26a2f24f6fef94c82f8f58
Vingnir <vingnir@live.se>
2022-01-27 17:23:56 +0100

    added about section

---
+++ LexiconMVC/wwwroot/js/main.js
@@ -0,0 +1,39 @@
+﻿// utilities
+var get = function (selector, scope) {
+    scope = scope ? scope : document;
+    return scope.querySelector(selector);
+};
+
+var getAll = function (selector, scope) {
+    scope = scope ? scope : document;
+    return scope.querySelectorAll(selector);
+};
+
+// setup typewriter effect in the terminal demo
+if (document.getElementsByClassName('demo').length > 0) {
+    var i = 0;
+    var txt = `Core information... 
+            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
+
+            #TODO: Get a garden, play more with the dogs, write even better code 
+
+            - Name = Johan Daniel Ivarsson
+            - Age = 40
+            - Location = Gamlestaden, Gothenburg
+            - Interests = Dogs, plants and coding
+            - Languages = C#, JS, PHP, Java, SQL
+            - Tech = HTML, CSS, SASS, XML, JSON ...
+            - Creative solutions? true
+            - Fullstack = true`;
+    var speed = 60;
+
+    function typeItOut() {
+        if (i < txt.length) {
+            document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeItOut, speed);
+        }
+    }
+
+    setTimeout(typeItOut, 1800);
+}
---
+++ LexiconMVC/wwwroot/js/portfolio.js
@@ -0,0 +1,137 @@
+/**
+* Template Name: Arsha - v4.7.1
+* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+ 
+
+  
+  
+  /**
+   * Initiate  glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
